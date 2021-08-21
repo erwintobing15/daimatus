6026a8bc6caa92f26e2f05d2e9ab19f32e19648e
erwintobing15 <erwintobing15@gmail.com>
2021-08-21 23:08:33 +0700

    Create navbar and add it's style

--- index.php
+++ index.php
@@ -13,6 +13,28 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    
+    <!-- navigation  -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="images/logo.png" alt="" width="40" height="34" class="d-inline-block align-text-top">
+          Daimatus
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerClient" aria-controls="navbarTogglerClient" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerClient">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#" style="color: #fff;">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" style="color: #fff;">Mata Pelajaran</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
 </body>
 </html>
