f68ba56948f987d9715025a7d2e93e3b8d07fc4d
erwintobing15 <erwintobing15@gmail.com>
2021-08-22 00:15:16 +0700

    Break index.php to header, nav, content, footer

--- index.php
+++ index.php
@@ -1,41 +1,16 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daimatus - Daring Inovasi Mandiri Untuk Siswa</title>
-    <link rel="icon" href="images/logo.png" type="image/png"/>
-    <!-- bootstrap v5.0 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <!-- custom css -->
-    <link rel="stylesheet" href="css/main.css">
-</head>
-<body>
-    <!-- navigation  -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          <img src="images/logo.png" alt="" width="40" height="34" class="d-inline-block align-text-top">
-          Daimatus
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerClient" aria-controls="navbarTogglerClient" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerClient">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="#" style="color: #fff;">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" style="color: #fff;">Mata Pelajaran</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+<?php  
+    // load header
+    require_once("templates/header.php");
+
+    // load navbar
+    require_once("templates/navbar.php");
+
+    // load content
 
 
-</body>
-</html>
+    // load footer
+    require_once("templates/footer.php");
+?>
+
+
+
