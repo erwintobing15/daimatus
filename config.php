698958124a3c6cf01c0413983576150ab7ca695f
erwintobing15 <erwintobing15@gmail.com>
2021-08-26 01:51:57 +0700

    Define global path in config.php and applied them

--- config.php
+++ config.php
@@ -1,11 +1,24 @@
 <?php
 
-    // database configuration
+    /* create database configuration*/
     $database = array(
         'host' => 'root',
         'username' => 'localhost',
         'password' => '',
         'name' => 'daimatus',
     );
+
+    /* define global path directory */
+    defined ('IMAGES_PATH')
+        or define('IMAGES_PATH', realpath(dirname(__FILE__) . '/images'));
+
+    defined ('LIBRARY_PATH')
+        or define('LIBRARY_PATH', realpath(dirname(__FILE__) . '/library'));
+
+    defined ('TEMPLATES_PATH')
+        or define('TEMPLATES_PATH', realpath(dirname(__FILE__) . '/templates'));
+
+        
+
 
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,26 @@
 <?php  
+    // load config
+    require_once("config.php");
+    
     // load header
-    require_once("templates/header.php");
+    require_once(TEMPLATES_PATH . "/header.php");
 
     // load navbar
-    require_once("templates/navbar.php");
+    require_once(TEMPLATES_PATH . "/navbar.php");
 
     // website content
     if (!isset($_GET['content'])) {
-      require_once("templates/home.php");
+      require_once(TEMPLATES_PATH . "/home.php");
     } else {
       switch($_GET['content']) {
-        case 'home' :  require_once("templates/home.php"); break;
-        case 'matpel' :  require_once("templates/matpel.php"); break;
-        default:  require_once("templates/home.php");
+        case 'home' :  require_once(TEMPLATES_PATH . "/home.php"); break;
+        case 'matpel' :  require_once(TEMPLATES_PATH . "/matpel.php"); break;
+        default:  require_once(TEMPLATES_PATH . "/home.php");
       }
     } 
 
     // load footer
-    require_once("templates/footer.php");
+    require_once(TEMPLATES_PATH . "/footer.php");
 ?>
 
 
