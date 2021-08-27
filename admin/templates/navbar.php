8b2253ccc6f4e7e4b5c9ddcf2c00f2aa71da0d24
erwintobing15 <erwintobing15@gmail.com>
2021-08-27 13:36:32 +0700

    Update style admin/main.css

--- admin/templates/navbar.php
+++ admin/templates/navbar.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
     <h2 onclick="location.replace('http://localhost/daimatus/')">Website</h2>
     <ul>
-        <li><a href="?content=home"><i class="fas fa-home"></i> Home</a></li>
-        <li><a href="?content=user"><i class="fas fa-user"></i> User</a></li>
+      <li><a href="?content=home"><i class="fas fa-home"></i>Home</a></li>
+      <li><a href="?content=user"><i class="fas fa-user"></i>Profile</a></li>
     </ul>
 </div>
