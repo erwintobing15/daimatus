1fd98e246baf9a7b664c99a0de1ac3a4690107f5
erwintobing15 <erwintobing15@gmail.com>
2021-08-27 23:29:54 +0700

    Rename all templates file and move content out

--- index.php
+++ index.php
@@ -1,27 +1,16 @@
-<?php  
+<?php
     // load config
     require_once("config.php");
-    
+
     // load header
-    require_once(TEMPLATES_PATH . "/header.php");
+    require_once(TEMPLATES_PATH . "/header.html");
 
     // load navbar
-    require_once(TEMPLATES_PATH . "/navbar.php");
+    require_once(TEMPLATES_PATH . "/navbar.html");
 
     // website content
-    if (!isset($_GET['content'])) {
-      require_once(TEMPLATES_PATH . "/home.php");
-    } else {
-      switch($_GET['content']) {
-        case 'home' :  require_once(TEMPLATES_PATH . "/home.php"); break;
-        case 'matpel' :  require_once(TEMPLATES_PATH . "/matpel.php"); break;
-        default:  require_once(TEMPLATES_PATH . "/home.php");
-      }
-    } 
+    require_once(TEMPLATES_PATH . "/content.html");
 
     // load footer
-    require_once(TEMPLATES_PATH . "/footer.php");
+    require_once(TEMPLATES_PATH . "/footer.html");
 ?>
-
-
-
