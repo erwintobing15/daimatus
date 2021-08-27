7e0541139f9ff39f4c6f3def37b774a1b725ebac
erwintobing15 <erwintobing15@gmail.com>
2021-08-27 13:02:46 +0700

    Update admin navbar style

--- admin/templates/content.php
+++ admin/templates/content.php
@@ -4,6 +4,16 @@
         <a href="library/action-logout.php" id="right-header"><i class="fa fa-sign-out" style="font-size:18px;color:#009fe1;">Logout</i></a>
     </div>
     <div class="info">
-
+      <?php
+        if (!isset($_GET['content'])) {
+          require_once(ADMIN_TEMPLATES_PATH . "/home.php");
+        } else {
+          switch($_GET['content']) {
+            case 'home' :  require_once(ADMIN_TEMPLATES_PATH . "/home.php"); break;
+            case 'user' :  require_once(ADMIN_TEMPLATES_PATH . '/user.php'); break;
+            default:  require_once(ADMIN_TEMPLATES_PATH . "/home.php");
+          }
+        }
+      ?>
     </div>
 </div>
