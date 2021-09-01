e2ec9d17465b1698bc06b5ffd217bbad7ea728d4
erwintobing15 <erwintobing15@gmail.com>
2021-09-01 12:47:13 +0700

    Create backend logic to display quiz in quiz.html

---
+++ admin/library/quiz.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * class Quiz that handle database process to display,
+ * input new quiz, update and delete quiz
+ */
+ class Quiz
+ {
+   private $conn;
+
+   function __construct($host,$username,$password,$name)
+   {
+     // create database connection
+     $db = new DbConnection($host,$username,$password,$name);
+     $this->conn = $db->connect();
+   }
+
+   // database handler to get quiz by matpel and topic
+   public function getQuizByMatpelTopic($matpelId,$topicId) {
+     $sql = "SELECT * FROM quiz
+             WHERE matpel_id = '".$matpelId."'
+             AND topic_id = '".$topicId."'";
+     $result = $this->conn->query($sql);
+
+     return $result;
+   }
+ }
+?>
