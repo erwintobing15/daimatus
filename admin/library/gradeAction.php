928b9ddff47927d8643e0fa6fd1735ac54096454
erwintobing15 <erwintobing15@gmail.com>
2021-09-07 13:01:17 +0700

    Remove add grade action from quiz.html to gradeAction.php

--- admin/library/gradeAction.php
+++ admin/library/gradeAction.php
@@ -6,14 +6,35 @@
 
   $grade = new Grade($database['host'],$database['username'],$database['password'],$database['name']);
 
+
+  /* action if there is a submit to add grade */
+  if (isset($_POST['addGrade'])) {
+
+    $materiId = $_POST['materi-id'];
+    $subTopic = $_POST['sub-topic'];
+    $matpelId = $_POST['matpel-id'];
+    $topicId = $_POST['topic-id'];
+
+    $studentName = $_POST['student-name'];
+    $studentId = $_POST['student-id'];
+    $studentClass = $_POST['student-class'];
+    $studentGrade = $_POST['student-grade'];
+
+    $message = $grade->addGrade($subTopic,$studentName,$studentId,$studentClass,
+                                $studentGrade,$matpelId,$topicId);
+
+    header("Location: ../../?content=quizresult&materi_id=$materiId&student_id=$studentId");
+    die;
+  }
+
   if (isset($_POST['delGrade'])) {
     $gradeId = $_POST['delGradeId'];
 
     $message = $grade->delGrade($gradeId);
-  }
 
-  // redirect to grade page
-  header("Location: ../?content=grade");
-  die;
+    // redirect to grade page
+    header("Location: ../?content=grade");
+    die;
+  }
 
 ?>
