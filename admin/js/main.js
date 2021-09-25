9870f1b76377d1be76984799d44ae0c12000761a
erwintobing15 <erwintobing15@gmail.com>
2021-09-25 14:23:13 +0700

    Create branch assets and add images

--- admin/js/main.js
+++
@@ -1,7 +0,0 @@
-/*
-  script to pass user id, matpel id, topic id etc to a hidden
-  input inside a modal form
-*/
-function passId(user_id, elmnt_id) {
-  document.getElementById(elmnt_id).value = user_id;
-}
